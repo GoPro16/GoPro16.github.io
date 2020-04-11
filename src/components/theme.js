dc58e2334b94d999d6c9656289f2bd8a0233dac8
Kyle Gray <kgray1497@gmail.com>
2020-04-11 19:29:17 -0400

    refactor: try init state again

--- src/components/theme.js
+++ src/components/theme.js
@@ -16,14 +16,18 @@ const defaultTheme = "dark";
 const ThemeContext = createContext([null, null]);
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(null);
+  const [theme, setTheme] = useState(() => {
+    try {
+      return window.__theme;
+    } catch (err) {
+      return "dark";
+    }
+  });
 
   useEffect(() => {
     window._onThemeChange = () => {
       setTheme(window.__theme);
     };
-
-    setTheme(window.__theme || "dark");
   }, []);
 
   const cssModule = useMemo(() => themeConfig(theme === "dark"), [theme]);
@@ -43,21 +47,20 @@ const ThemeProvider = ({ children }) => {
           })
       ]}
     >
-      {theme !== null &&
-        (typeof children === "function"
-          ? children({
-              theme,
-              cssModule,
-              changeTheme: () =>
-                setTheme(t => {
-                  const nextState = t === "dark" ? "light" : "dark";
+      {typeof children === "function"
+        ? children({
+            theme,
+            cssModule,
+            changeTheme: () =>
+              setTheme(t => {
+                const nextState = t === "dark" ? "light" : "dark";
 
-                  window.__setPreferredTheme(nextState);
+                window.__setPreferredTheme(nextState);
 
-                  return nextState;
-                })
-            })
-          : children)}
+                return nextState;
+              })
+          })
+        : children}
     </ThemeContext.Provider>
   );
 };
